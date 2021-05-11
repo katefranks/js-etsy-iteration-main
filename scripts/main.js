5487efd06207d48cbe9b7f656379136e02c9c918
katefranks <kmfranks9@gmail.com>
2021-05-11 17:54:39 -0400

    step 3 partially completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,6 @@
-// let total = 0;
-// items['price']forEach(function (prices){
-//   total += 'price';
-// }
-// return total;
-// });
 
-//
-// items.forEach(function(item){
-//   console.log(item.price);
-// });
-//
 //////////// STEP 1 /////////////
+//final solution:
 
 // const pricesArray = items.map(function(item){
 //   return item.price;
@@ -24,7 +14,35 @@
 //
 // console.log(avgPrices);
 
-//////////// STEP 2 /////////////
+/////////////////////
+//step 1 trial & error below:
+// let total = 0;
+// items['price']forEach(function (prices){
+//   total += 'price';
+// }
+// return total;
+// });
+
+//
+// items.forEach(function(item){
+//   console.log(item.price);
+// });
+//
+
+////////////// STEP 2 ////////////////////////
+//final solution:
+
+
+  // let lower = 14;
+  // let upper = 18;
+  // let filterPrices = items.filter(function(item) {
+  //   return (item.price >= lower && item.price <= upper);
+  // });
+  //
+  // console.log(filterPrices);
+////////////////////////////////////////////////
+
+//step 2 trial & error below....
 
 //"Items that cost between $14.00 USD and $18.00 USD:"//
 // const pricesArray = items.map(function(item){
@@ -37,23 +55,7 @@
 //
 // })
 // //////////////////////////////
-// function isBigEnough(value) {
-//   return value >= 10
-// }
-//
-// let filtered = [12, 5, 8, 130, 44].filter(isBigEnough)
-// filtered is [12, 130, 44]
 /////////////////////////
-
-
-  let lower = 14;
-  let upper = 18;
-  let filterPrices = items.filter(function(item) {
-    return (item.price >= lower && item.price <= upper);
-  });
-
-  console.log(filterPrices);
-//////////////////////////
 
 // let lower = 14;
 // let upper = 18;
@@ -63,3 +65,23 @@
 //     }
 // }
 // console.log(filterByID);
+
+
+
+////////////// STEP 3 ////////////////////////
+//find the item with a "GBP" currency code
+//// The following console.logs the entire object w/ the GBP currency.
+let currency = "GBP";
+let filterCurrency = items.filter(function(item) {
+  return item.currency_code == currency;
+});
+//
+console.log(filterCurrency);
+
+///// Trying to figure out how to show just the title & price.
+
+// let a = filterCurrency;
+//
+// console.log(a.title);
+
+// console.log(filterCurrency['title']);
